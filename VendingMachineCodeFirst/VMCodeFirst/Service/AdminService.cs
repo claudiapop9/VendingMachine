cfbc0e99f087556ba2d4f8e2896b312032024917
popclaudia <popclaudia97@gmail.com>
2019-09-26 17:09:36 +0300

    service divided in two client and admin-SRP

---
+++ VendingMachineCodeFirst/VMCodeFirst/Service/AdminService.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace VendingMachineCodeFirst.Service
+{
+    public class AdminService
+    {
+        private readonly IProductRepository productRepository = new ProductRepository();
+        private readonly DataRepository dataStorage = new DataRepository();
+        private readonly ReportRepository report = new ReportRepository();
+        private readonly TransactionService transaction = new TransactionService();
+
+        internal void AddProduct(Product p)
+        {
+            this.productRepository.AddProduct(p);
+            dataStorage.PersistData(this.productRepository.GetProducts());
+        }
+
+        internal void UpdateProduct(Product p)
+        {
+            this.productRepository.UpdateProduct(p);
+        }
+
+        internal void DeleteProduct(int productId)
+        {
+            this.productRepository.RemoveProduct(productId);
+            dataStorage.PersistData(this.productRepository.GetProducts());
+        }
+
+        public bool Refill()
+        {
+            IList<Product> productsToRefList = productRepository.GetProductsToRefill();
+            if (productRepository.Refill())
+            {
+                dataStorage.PersistData(this.productRepository.GetProducts());
+                transaction.AddTransactionRefill(productsToRefList);
+                return true;
+            }
+            return false;
+        }
+
+        public string GenerateReport()
+        {
+            List<Transaction> transactions = transaction.GetTransactions();
+            return report.GenerateReport(transactions);
+        }
+
+        public IList<Product> GetProducts()
+        {
+            return productRepository.GetProducts();
+        }
+    }
+}
---
+++ VendingMachineCodeFirst/VMCodeFirst/Service/ClientService.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace VendingMachineCodeFirst.Service
+{
+    public class ClientService
+    {
+        private readonly IProductRepository productRepository = new ProductRepository();
+        private DataRepository dataStorage = new DataRepository();
+        private TransactionService transactionService = new TransactionService();
+        private IPayment payment;
+
+        public ClientService() { }
+
+        public ClientService(IPayment paymentMethod, IProductRepository productRepository)
+        {
+            this.payment = paymentMethod;
+            this.productRepository = productRepository;
+        }
+        public ClientService(IPayment paymentMethod)
+        {
+            this.payment = paymentMethod;
+        }
+
+        public bool BuyProduct(int productId)
+        {
+            double productPrice = productRepository.GetProductPriceByKey(productId);
+            if (payment.IsValid() && payment.IsEnough(productPrice))
+            {
+                productRepository.DecreaseProductQuantity(productId);
+                payment.Pay(productPrice);
+                dataStorage.PersistData(GetProducts());
+                transactionService.AddTransition("BUY", productId);
+                return true;
+            }
+            return false;
+        }
+
+        public IList<Product> GetProducts()
+        {
+            return productRepository.GetProducts();
+        }
+    }      
+}
